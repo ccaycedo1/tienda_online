d46c76ac757eefceb7a75ebdae2f07ad68bd0ff4
Cristhian Caycedo <cacaycedo@gov.co>
2023-12-22 08:03:16 -0500

    ajustes

---
+++ src/components/bernes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const bernes = () => {
+  return (
+    <div>bernes</div>
+  )
+}
---
+++ src/components/yorkshire.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const yorkshire = () => {
+  return (
+    <div>labrador</div>
+  )
+}
